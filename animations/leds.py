54dd2dab606f1e944d52ae1a0843b0cb6777e01b
Timmy Scott <timjimski@gmail.com>
2015-07-02 02:43:14 +0000

    Added debug code to test speed of animations.

--- animations/leds.py
+++ animations/leds.py
@@ -1,5 +1,6 @@
 from stars import Stars
 from staticlight import StaticLight
+from fire import Fire
 from constants import *
 #from ledstrip import NeoPixelWithAlpha
 from neopixel import *
@@ -15,22 +16,28 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-stars = Stars(strip, 30, 1, 100, 255, 100)
+stars = Stars(strip, 15, 1, 255, 255, 255)
 #150-225
-light = StaticLight(strip, 1, 150, 225)
+#light = StaticLight(strip, 150, 225, 255, 40, 0)
+light = StaticLight(strip, 0, LED_COUNT, 63, 12, 0)
+fire = Fire(strip, 140, 1, 5, 10)
 absoluteStart = time.time()
-
-while 1:
+totalSleepTime = 0
+for i in range(0, 300):
+#while 1:
     start = time.time()
     #light.animate()
-    stars.animate()
+    #stars.animate()
     #light.animate()
+    fire.animate()
     strip.show()
     ticks += 1
     sleepTime = 0.033 - (time.time() - start)
     if(sleepTime < 0):
         sleepTime = 0
+    totalSleepTime += sleepTime
     time.sleep(sleepTime)
 
 absoluteEnd = time.time()
 print "Ticks per second: " + str(ticks / (absoluteEnd - absoluteStart))
+print "Proportion spent sleeping: " + str(totalSleepTime / (absoluteEnd - absoluteStart))
