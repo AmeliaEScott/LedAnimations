55a64a948dbd86e3bbabe4d34f4e93626feba9aa
ItsTimaiFool <timjimski@gmail.com>
2015-08-06 22:20:59 -0400

    Delete wraparoundstrip.py

--- animations/wraparoundstrip.py
+++
@@ -1,8 +0,0 @@
-from neopixel import *
-from constants import *
-
-class WrapAroundStrip(Adafruit_NeoPixel):
-
-    def setPixelColor(self, index, color):
-        index = index % LED_COUNT
-        super(WrapAroundStrip, self).setPixelColor(index, color)
