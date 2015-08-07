a69763cc8e08c5223b985de77cf6473922b59e0e
Timmy Scott <timjimski@gmail.com>
2015-08-07 01:47:06 +0000

    Fixed more annoying bugs in a rather kludgy way.

--- animations/fairy.py
+++ animations/fairy.py
@@ -17,12 +17,12 @@ class Fairy:
 
     def animate(self):
         self.tick += 1
-        dist = (self.tick * self.speed) + self.start
+        dist = ((self.tick * self.speed) + self.start) % LED_COUNT
         fraction = dist - (int(dist))
         dist = int(dist)
         #fraction = fraction ** 0.5
         startFraction = 1 - fraction
-        for i in range(1, int(self.speed) + 1) or [1]:
+        for i in range(1, int(self.speed) + 2) if int(self.speed) > 1 else range(int(self.speed) - self.width, self.width):
             self.strip.setPixelColor(dist - i, 0)
         self.strip.setPixelColor(dist, Color(int(startFraction * self.red), int(startFraction * self.green), int(startFraction * self.blue)))
         for i in range(1, self.width):
