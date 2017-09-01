4c18aad0580314a0559a9d161f5e87bef30ba8f5
Timmy <timjimski@gmail.com>
2017-08-31 22:03:18 -0400

    Attempting to fix byte order problems

--- LedServer/ledserver.py
+++ LedServer/ledserver.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 try:
     from neopixel import *
+    import _rpi_ws281x as ws
 except ImportError:
     from .dummy_neopixel import *
 import os
@@ -21,12 +22,23 @@ LED_FREQ_HZ = settings["strip_settings"]["frequency"]
 LED_DMA = settings["strip_settings"]["dma"]
 LED_BRIGHTNESS = settings["strip_settings"]["brightness"]
 LED_INVERT = settings["strip_settings"]["invert"]
+strip_type = settings["strip_settings"]["type"].lower()
+types = {
+    'rgb': ws.WS2811_STRIP_RGB,
+    'rbg': ws.WS2811_STRIP_RBG,
+    'grb': ws.WS2811_STRIP_GRB,
+    'gbr': ws.WS2811_STRIP_GBR,
+    'brg': ws.WS2811_STRIP_BRG,
+    'bgr': ws.WS2811_STRIP_BGR,
+}
+STRIP_TYPE = types[strip_type]
 
 
 class LedServer:
 
     def __init__(self):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        strip = Adafruit_NeoPixel(num=LED_COUNT, pin=LED_PIN, freq_hz=LED_FREQ_HZ, dma=LED_DMA, invert=LED_INVERT,
+                                  brightness=LED_BRIGHTNESS, strip_type=STRIP_TYPE)
         strip.begin()
 
         try:
