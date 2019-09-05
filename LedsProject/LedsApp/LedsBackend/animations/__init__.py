5bc2c1ce7f4d71dbda94da0d59e312e8dfe10cea
Timmy <timjimski@gmail.com>
2019-09-05 13:10:18 -0400

    Added rainbow animation

--- LedsProject/LedsApp/LedsBackend/animations/__init__.py
+++ LedsProject/LedsApp/LedsBackend/animations/__init__.py
@@ -2,5 +2,6 @@ from .fairy import Fairy
 from .staticlight import StaticLight
 from .fire import Fire
 from .stars import Stars
+from .rainbow import Rainbow
 
-animationclasses = [Fairy, StaticLight, Fire, Stars]
+animationclasses = [Fairy, StaticLight, Fire, Stars, Rainbow]
---
+++ LedsProject/LedsApp/LedsBackend/animations/rainbow.py
@@ -0,0 +1,49 @@
+from ..plugins import Animation, AnimationParameter
+import time
+import colorsys
+
+
+class Rainbow(Animation):
+    
+    def __init__(self, id, options):
+        super().__init__(id, options=options)
+        self.start = options['start']
+        self.end = options['end']
+        self.length = options['length']
+        self.speed = options['speed']
+        self.brightness = options['brightness']
+        self.saturation = options['saturation']
+
+        self.tick = 0
+
+    def animate(self, delta, strip):
+        self.tick += delta
+        hue = self.tick * self.speed
+        for i in range(self.start, self.end):
+            strip.setPixelColor(i, rgb=colorsys.hsv_to_rgb(hue, self.saturation, self.brightness))
+            hue = (hue + 1 / self.length) % 1.0
+        return
+
+    @staticmethod
+    def getparams():
+        return [
+            AnimationParameter('start', description="Where the rainbow starts",
+                               type='integer', optional=False),
+            AnimationParameter('end', description="Where the rainbow ends",
+                               type='float', optional=False),
+            AnimationParameter('length',
+                               description="The rainbow will make one trip around the color wheel in this length",
+                               type='integer', optional=False),
+            AnimationParameter('speed', description='How fast to move the colors',
+                               type='float', optional=False, default=0),
+            AnimationParameter('brightness', description="Brightness of the whole rainbow (0 to 1)",
+                               type='float', default=1, optional=False),
+            AnimationParameter('saturation', description='Saturation (0 to 1)', advanced=True, default=1, type='float')
+        ]
+
+    @staticmethod
+    def getanimationinfo():
+        return {
+            'name': 'Rainbow',
+            'description': 'A rainbow that optionally moves'
+        }
