098342ef389028934d6067f863c27751dad046cb
Timmy <timjimski@gmail.com>
2019-09-09 15:22:57 -0400

    Uncommented important stuff

--- LedServer/ledserver.py
+++ LedServer/ledserver.py
@@ -1,9 +1,9 @@
 from __future__ import print_function, division
-# try:
-#     from neopixel import *
-#     import _rpi_ws281x as ws
-# except ImportError:
-#     from .dummy_neopixel import *
+try:
+    from neopixel import *
+    import _rpi_ws281x as ws
+except ImportError:
+    from .dummy_neopixel import *
 import os
 import time
 import json
@@ -16,7 +16,6 @@ import numpy as np
 with open(os.path.join(os.path.dirname(__file__), '..', 'settings.json')) as settingsfile:
     settings = json.load(settingsfile)
 
-PIPE_PATH = settings["pipe_name"]
 ZMQ_PORT = settings["zmq_port"]
 LED_COUNT = settings["strip_settings"]["length"]
 LED_PIN = settings["strip_settings"]["pin"]
@@ -25,15 +24,15 @@ LED_DMA = settings["strip_settings"]["dma"]
 LED_BRIGHTNESS = settings["strip_settings"]["brightness"]
 LED_INVERT = settings["strip_settings"]["invert"]
 strip_type = settings["strip_settings"]["type"].lower()
-# types = {
-#     'rgb': ws.WS2811_STRIP_RGB,
-#     'rbg': ws.WS2811_STRIP_RBG,
-#     'grb': ws.WS2811_STRIP_GRB,
-#     'gbr': ws.WS2811_STRIP_GBR,
-#     'brg': ws.WS2811_STRIP_BRG,
-#     'bgr': ws.WS2811_STRIP_BGR,
-# }
-# STRIP_TYPE = types[strip_type]
+types = {
+    'rgb': ws.WS2811_STRIP_RGB,
+    'rbg': ws.WS2811_STRIP_RBG,
+    'grb': ws.WS2811_STRIP_GRB,
+    'gbr': ws.WS2811_STRIP_GBR,
+    'brg': ws.WS2811_STRIP_BRG,
+    'bgr': ws.WS2811_STRIP_BGR,
+}
+STRIP_TYPE = types[strip_type]
 
 running = True
 
@@ -84,7 +83,7 @@ while running:
     # Convert to the form that the LED strip needs: a 32-bit integer
     data_packed = (data[:, 0] << 16) | (data[:, 1] << 8) | (data[:, 2])
 
-    # TODO: Is there a more efficient way to copy over the colors?
+    # TODO: Is there a more efficient way to copy over the colors? Probably
     for i in range(0, data_packed.shape[0]):
         strip.setPixelColor(i, data_packed[i])
 
